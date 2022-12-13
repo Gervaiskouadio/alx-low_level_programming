725287cf388671f2745279b130ec3cd3cd5ec642
Gervaiskouadio <lelagervais1@gmail.com>
2022-12-13 03:01:17 -0800

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,20 @@
 #include <unfstd.h>
 
 /**
- * _putchar -writes the character c to stdout
- * @c: the character to print
- *
- * Return: on success 1
+ * main - print _putchar and new line
+ * Return: Always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
