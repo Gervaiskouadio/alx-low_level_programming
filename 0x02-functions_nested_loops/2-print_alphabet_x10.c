bd38e3f28128132a17e1c42c94722867cd27bb7c
Gervaiskouadio <lelagervais1@gmail.com>
2022-12-13 14:30:45 -0800

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,14 @@
 /**
  * print_alphabet_x10 - print alphabets 10 times
  */
-void pint_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int ten;
 	char le;
 
 	for (ten = 0; ten <= 9; ten++)
 	{
-		for (le = 'a'; le <= 'z')
+		for (le = 'a'; le <= 'z';)
 			_putchar(le);
 		_putchar('\n');
 	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,7 +13,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void prin_to_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
 void num(int r,int n);
 
