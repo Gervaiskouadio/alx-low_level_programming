08494c122ffdbe57128123a6c89856c8580dceef
Gervaiskouadio <lelagervais1@gmail.com>
2022-12-17 07:14:43 -0800

    int _isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,19 +3,13 @@
 /**
  * _isdigit - check if numbers are 0 - 9
  * @c: character to check
- * Return: 0 or 1
+ * Return: 1 or 0
  */
 
-int_isdigit(int c)
+int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
-		_putchar(c);
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
-
