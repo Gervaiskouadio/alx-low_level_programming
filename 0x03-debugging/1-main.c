925abc54605fef26a3f2e9f4a967def8899f09ec
Gervaiskouadio <lelagervais1@gmail.com>
2022-12-20 15:16:39 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - causes an infinite loop
@@ -13,6 +14,7 @@ int main(void)
 	i = 0;
 	while (i < 10)
 	{
+		putchar(i);
 		printf("Infinite loop avoided! \\o/\n");
 		return (0);
 	}
