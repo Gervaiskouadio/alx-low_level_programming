baa847eb6eb717923b57992330bb8ea920f8897d
Gervaiskouadio <lelagervais1@gmail.com>
2023-01-03 16:07:14 -0800

    task7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <bits/stdio_lim.h>
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ * Return: 0 (success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
+}
